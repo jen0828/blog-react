c2757b0f0f6817988249e35d16e8718fb40064a6
Jenny Moon <jenny0moon@gmail.com>
2021-10-13 16:07:42 +0100

    Use cleanup function to remove useEffect error

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then(res => {
           if (!res.ok) {
             throw Error('could not fetch the data for that resource')
@@ -20,11 +22,20 @@ const useFetch = (url) => {
           setError(null)
         })
         .catch(err => {
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted')
+          } else {
+            // auto catches network / connection error
+            setIsPending(false);
+            setError(err.message);
+          }
         })
-    })
+    });
+
+    // abort the fetch
+    return () => abortCont.abort();
   }, [url]);
+
 
   return { data, isPending, error };
 }
