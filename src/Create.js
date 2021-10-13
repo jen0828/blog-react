8b54fb6cf57400da933ce616b5fdfbc6c3b1968c
Jenny Moon <jenny0moon@gmail.com>
2021-10-13 18:54:20 +0100

    Redirect route using usehistory hook

--- src/Create.js
+++ src/Create.js
@@ -1,21 +1,23 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('author1');
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-
 
     fetch('http://localhost:8000/blogs/', {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
     }).then(() => {
-      console.log('new blog added');
+      // history.go(-1)
+      history.push('/');
     })
   }
 
