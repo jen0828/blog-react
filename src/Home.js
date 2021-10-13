bd1335747b99fe2d141626a102d069e3c8923f2f
Jenny Moon <jenny0moon@gmail.com>
2021-10-13 08:49:25 +0100

    Add delete blog function

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import BlogList from "./BlogList";
 
-
-
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'author1', id: 1 },
@@ -10,11 +8,15 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'author1', id: 3 }
   ])
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList blogs={blogs.filter(blog => blog.author === 'author1')} title="Mario's Blogs" />
-        </div>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete}/>
+    </div>
   );
 }
 
