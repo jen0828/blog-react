9b02c887952493a9471ff584b6fe2d16f4183e20
Jenny Moon <jenny0moon@gmail.com>
2021-10-13 15:27:15 +0100

    Fix bug

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
-  const [data, setBlogs] = useState(null)
+  const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
