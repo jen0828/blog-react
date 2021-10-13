fa98c1b27362dabf06cba260bd26d37f80ce6553
Jenny Moon <jenny0moon@gmail.com>
2021-10-13 16:16:38 +0100

    Add route params

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar'
 import Home from './Home'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
@@ -16,11 +17,9 @@ function App() {
             <Route path="/create">
               <Create />
             </Route>
-
             <Route path="/blogs/:id">
               <BlogDetails />
             </Route>
-
           </Switch>
         </div>
       </div>
