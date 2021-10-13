f0f5405f4c01ebc56d38482576e503323a284e6e
Jenny Moon <jenny0moon@gmail.com>
2021-10-13 18:23:22 +0100

    Add handlesubmit in create.js

--- src/Create.js
+++ src/Create.js
@@ -5,14 +5,19 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('author1');
 
+  const handleSubmit = (e) => {
+    const blog = { title, body, author };
+    console.log(blog);
+  }
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
-        <input 
-          type="text" 
-          required 
+        <input
+          type="text"
+          required
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
@@ -35,5 +40,5 @@ const Create = () => {
     </div>
   );
 }
- 
+
 export default Create;
